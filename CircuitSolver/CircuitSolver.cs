cfaa3f7041a2f5d779f5864cf74b9b1e65545f19
micahspencer-png <micahspencer@isu.edu>
2026-02-26 14:25:43 -0700

    Add project files.

---
+++ CircuitSolver/CircuitSolver.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CircuitSolver
+{
+    //Micah Spencer
+    //RCET 3371
+    //Spring 2026
+    //Circuit Solver Program
+    //
+    public partial class CircuitSolver : Form
+    {
+        string ACPath = "..\\..\\..\\AC-SeriesParallel.png";
+        public CircuitSolver()
+        {
+            InitializeComponent();
+        }
+
+        private void CircuitSolver_Load(object sender, EventArgs e)
+        {
+            this.BackgroundImage = Image.FromFile(ACPath);
+        }
+    }
+}
